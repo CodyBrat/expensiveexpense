f67d4f40e66b258f858704840d8ab9685229a60b
priyabrata singh <priyabratasingh2005@gmail.com>
2025-04-12 15:46:57 +0530

    Update styles and structure in App component; enhance table layout and add color swatches for categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import  PocketBase from 'pocketbase'
+import PocketBase from 'pocketbase'
 
 function App() {
   const pb = new PocketBase('http://127.0.0.1:8090');
@@ -17,26 +17,30 @@ function App() {
 
   return (
     <>
-    <table className='table'>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Color</th>
-        </tr>
-      </thead>
-      <tbody>
-        {categories.map((category) => (
-          <tr key={category.id}>
-            <td>{category.id}</td>
-            <td>{category.name}</td>
-            <td style={{backgroundColor: category.color}}>{category.color}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-
-
+      <div className="container">
+        <h1 className="title">Expense Categories</h1>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Category Name</th>
+              <th>Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((category) => (
+              <tr key={category.id}>
+                <td>{category.id}</td>
+                <td>{category.name}</td>
+                <td className="color-cell">
+                  <span className="color-swatch" style={{backgroundColor: category.color}}></span>
+                  {category.color}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
